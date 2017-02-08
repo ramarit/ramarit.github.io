4f404c6dcbed63a7de8b54e184fcbc016d9413d4
Ryan Amarit <ramarit@gmail.com>
2017-02-07 18:35:09 -0800

    update form validation

--- js/jquery.js
+++ js/jquery.js
@@ -52,20 +52,46 @@ jQuery(document).ready(function($){
   });
 
 
+  // Validates the contact form and submits it using Formspree.
+  $.validator.setDefaults({
+    highlight: function(element) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'span',
+    errorClass: 'help-block',
+    errorPlacement: function(error, element) {
+        if(element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+  });
+
   $("#contactForm").validate({
-  submitHandler: function(form) {
-    $.ajax({
-      url: "//formspree.io/myemail@gmail.com", 
-      method: "POST",
-      data: {
-        name: $(form).find("input[name='name']").val(),
-        _replyto: $(form).find("input[name='_replyto']").val(),
-        message: $(form).find("textarea[name='message']").val()
-      },
-      dataType: "json"
-    });
-  }
-});
+    submitHandler: function(form) {
+      $.ajax({
+        url: "//formspree.io/myemail@gmail.com", 
+        method: "POST",
+        data: {
+          name: $(form).find("input[name='name']").val(),
+          _replyto: $(form).find("input[name='_replyto']").val(),
+          message: $(form).find("textarea[name='message']").val()
+        },
+        dataType: "json",
+        success: function() {
+          $("#submit-success").fadeIn();
+          $("#contact-form").fadeOut();
+        },
+        error: function() {
+          $("#submit-errors").fadeIn();        
+        }
+      });
+    }
+  });
 
 });
 
